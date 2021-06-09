0341a25161fb5590e575bf9dfcb9dd60b5efa66e
John Hwang <johnhchwang@gmail.com>
2021-06-08 17:47:49 -0700

    Completed issue-2

--- js/data.js
+++ js/data.js
@@ -1,1 +1,27 @@
 /* exported data */
+
+var data = {
+  drinks: []
+};
+
+// strDrink: null,
+// strInstructions: null,
+// strRecipe: null,
+// strDrinkThumb: null
+
+var prevData = {
+  drinks: []
+}
+
+// beforeunload, store existing prevData.drinks to local storage
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(prevData);
+  localStorage.setItem('prevData', dataJSON);
+});
+
+// on refresh, get previous data.entries and update current data.entries with it
+var dataItems = localStorage.getItem('prevData');
+var dataObj = JSON.parse(dataItems);
+
+// set previous prevData data to current prevData object
+prevData.drinks = dataObj.drinks;
